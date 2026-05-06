51181d696002e39b24ec9f6a1f1ed9ece05c7adf
stansam <stanleycmam122@gmail.com>
2026-05-06 03:16:12 +0300

    feat:order page ongoing

--- src/app/order/layout.tsx
+++ src/app/order/layout.tsx
@@ -6,6 +6,7 @@ import { QueryProvider } from "@/lib/providers/QueryProvider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "sonner";
 import { redirect } from "next/navigation";
+import type { AuthUser } from "@/lib/types/auth.type";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -20,15 +21,15 @@ export const metadata = {
 
 export default async function OrderLayout({ children }: { children: React.ReactNode }) {
   const authResult = await getServerAuthUser();
-  
-  if (!authResult.ok) {
-    redirect("/auth/login?callbackUrl=/order");
-  }
+  const initialUser: AuthUser | null = authResult.ok ? authResult.user : null;
+  // if (!authResult.ok) {
+  //   redirect("/auth/login?callbackUrl=/order");
+  // }
 
   return (
     <html lang="en" className={dmSans.variable}>
       <body className={`${dmSans.className} antialiased bg-[#e8e6e1]`}>
-        <AuthProvider initialUser={authResult.user} skipInitialFetch={true}>
+        <AuthProvider initialUser={initialUser} skipInitialFetch={initialUser !== null }>
           <QueryProvider>
             <TooltipProvider delayDuration={300}>
               <div className="flex min-h-screen flex-col">
